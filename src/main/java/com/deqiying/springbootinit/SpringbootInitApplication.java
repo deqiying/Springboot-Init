a7fcbb4e1789f7bf97df50abfa001949cc9ac8df
deqiying <deqiying@gmail.com>
2023-12-26 00:01:22 +0800

    init

---
+++ src/main/java/com/deqiying/springbootinit/SpringbootInitApplication.java
@@ -0,0 +1,13 @@
+package com.deqiying.springbootinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInitApplication.class, args);
+    }
+
+}
