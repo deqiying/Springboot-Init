bfc45a5a488c7921be7d3b3be3dd05933c8dfad1
deqiying <deqiying@gmail.com>
2024-01-07 22:47:58 +0800

    feat: web模块 引入统一接口返回格式实现

--- deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
+++ deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
@@ -1,8 +1,10 @@
 package com.deqiying.web;
 
+import com.deqiying.framework.annotation.EnableResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableResponse
 @SpringBootApplication
 public class DeqiyingWebApplication {
 
