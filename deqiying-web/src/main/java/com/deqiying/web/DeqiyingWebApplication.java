546b24b23bba084093fa3662549f28959173afc4
deqiying <deqiying@gmail.com>
2024-01-07 15:23:19 +0800

    refactor: 重命名 web 模块 包路径

--- deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
+++ deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
@@ -1,4 +1,4 @@
-package com.deqiying.deqiyingweb;
+package com.deqiying.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- deqiying-web/src/test/java/com/deqiying/web/DeqiyingWebApplicationTests.java
+++ deqiying-web/src/test/java/com/deqiying/web/DeqiyingWebApplicationTests.java
@@ -1,4 +1,4 @@
-package com.deqiying.deqiyingweb;
+package com.deqiying.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
