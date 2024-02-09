23a52c38c02193d6bc17c42d5c4575509bff1572
Qiying <deqiying@gmail.com>
2024-02-09 17:30:46 +0800

    feat: 集合工具类 -- 计算交集、并集、差集

--- deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
+++ deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
@@ -1,23 +1,34 @@
 package com.deqiying.common.utils;
 
-import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class CollUtils {
-    public static <T> Set<T> intersection(List<T> list1, List<T> list2) {
-        Set<T> set1 = new HashSet<>(list1);
-        Set<T> intersection = new HashSet<>();
+    // 计算交集
+    public static <T> List<T> intersection(List<T> list1, List<T> list2) {
+        Set<T> set2 = new HashSet<>(list2);
+        return list1.stream()
+                .filter(set2::contains)
+                .collect(Collectors.toList());
+    }
 
-        for (T element : list2) {
-            if (set1.contains(element)) {
-                intersection.add(element);
-            }
-        }
+    // 计算并集
+    public static <T> List<T> union(List<T> list1, List<T> list2) {
+        Set<T> set = new HashSet<>(list1);
+        set.addAll(list2);
+        return new ArrayList<>(set);
+    }
 
-        return intersection;
+    // 计算差集
+    public static <T> List<T> difference(List<T> list1, List<T> list2) {
+        Set<T> set2 = new HashSet<>(list2);
+        return list1.stream()
+                .filter(e -> !set2.contains(e))
+                .collect(Collectors.toList());
     }
 }
