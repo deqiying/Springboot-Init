cddf38793065965cb37ca8125a3de7dba201cd24
deqiying <deqiying@gmail.com>
2024-02-22 23:15:38 +0800

    feat: 集合相关常用算法

--- deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
+++ deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
@@ -1,14 +1,42 @@
 package com.deqiying.common.utils;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
-
+/**
+ * 集合相关常用算法
+ *
+ * @author deqiying
+ */
 public class CollUtils {
+
+    /**
+     * 数组转list
+     *
+     * @param list 集合
+     * @param <T>  泛型
+     * @return 数组
+     */
+    public static <T> T[] toArray(List<T> list) {
+        // 创建一个新的数组，大小为列表的大小
+        @SuppressWarnings("unchecked")
+        T[] array = (T[]) new Object[list.size()];
+        // 返回转换后的数组
+        return list.toArray(array);
+    }
+
+    /**
+     * 数组转集合
+     *
+     * @param array 数组
+     * @param <T>   泛型
+     * @return 集合
+     */
+    public static <T> List<T> asList(T[] array) {
+        return Arrays.asList(array);
+    }
+
     // 计算交集
     public static <T> List<T> intersection(List<T> list1, List<T> list2) {
         Set<T> set2 = new HashSet<>(list2);
