6d079f75b6721d7c0dc8287d906760c71db43757
Qiying <deqiying@gmail.com>
2024-02-27 16:29:55 +0800

    feat: web 模块 -- 引入 common 模块常用bean

--- deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
+++ deqiying-web/src/main/java/com/deqiying/web/DeqiyingWebApplication.java
@@ -1,10 +1,12 @@
 package com.deqiying.web;
 
+import com.deqiying.common.annotation.EnableCommon;
 import com.deqiying.framework.annotation.EnableResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableResponse
+@EnableCommon
 @SpringBootApplication
 public class DeqiyingWebApplication {
 
