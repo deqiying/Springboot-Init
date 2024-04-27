6cb891a5001e0f739b535551d6c5b2db148488c3
deqiying <deqiying@gmail.com>
2024-04-27 19:10:49 +0800

    chore: 分页实体修改

--- deqiying-framework/src/main/java/com/deqiying/framework/vo/PageVO.java
+++ deqiying-framework/src/main/java/com/deqiying/framework/vo/PageVO.java
@@ -34,10 +34,6 @@ public class PageVO<T> {
      * 分页大小
      */
     private Serializable pageSize;
-    /**
-     * 服务响应时间戳
-     */
-    private Long timestamp;
 
     public PageVO(List<T> records, Serializable total, Serializable pages, Serializable current, Serializable pageSize) {
         this.records = records;
@@ -45,7 +41,6 @@ public class PageVO<T> {
         this.pages = pages;
         this.current = current;
         this.pageSize = pageSize;
-        this.timestamp = System.currentTimeMillis();
     }
 
     public static <T> PageVO<T> page(List<T> records, Serializable total, Serializable pages, Serializable current, Serializable pageSize) {
