31ce5c6dd4b0320505a85c65ff264f96e6c07786
deqiying <deqiying@gmail.com>
2024-05-30 15:38:45 +0800

    feat: common模块 ProbabilityUtils 随机时间发生概率

---
+++ deqiying-common/src/main/java/com/deqiying/common/utils/ProbabilityUtils.java
@@ -0,0 +1,45 @@
+package com.deqiying.common.utils;
+
+import java.util.Random;
+
+public class ProbabilityUtils {
+    private static final Random random = new Random();
+
+    /**
+     * 判断概率事件是否发生
+     *
+     * @param numerator 事件发生的概率，应该是0到1之间的一个小数
+     * @return 如果事件发生，返回true；否则返回false
+     */
+    public static boolean fraction(double numerator) {
+        if (numerator < 0) {
+            return false;
+        }
+        if (numerator >= 1) {
+            return true;
+        }
+        return random.nextDouble() < numerator;
+    }
+
+    public static boolean percent(int numerator) {
+        if (numerator < 0) {
+            return false;
+        }
+        if (numerator >= 100) {
+            return true;
+        }
+        return random.nextInt(100) < numerator;
+    }
+
+    public static boolean permille(int numerator) {
+        if (numerator < 0) {
+            return false;
+        }
+        if (numerator >= 10000) {
+            return true;
+        }
+        return random.nextInt(10000) < numerator;
+    }
+
+
+}
