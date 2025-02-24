77c2532241c81860db34f5f5cf7b958dc4a0f757
deqiying <deqiying@gmail.com>
2025-02-24 22:50:21 +0800

    feat:(CollUtils): 优化 CollUtils,新增判断集合是否为空的方法

--- deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
+++ deqiying-common/src/main/java/com/deqiying/common/utils/CollUtils.java
@@ -111,4 +111,64 @@ public class CollUtils {
         return newlist;
     }
 
+    /**
+     * 判断数组是否为空
+     *
+     * @param array 数组
+     * @return 是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
+    /**
+     * 判断数组是否不为空
+     *
+     * @param array 数组
+     * @return 是否不为空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !isEmpty(array);
+    }
+
+    /**
+     * 判断集合是否为空
+     *
+     * @param collection 集合
+     * @return 是否为空
+     */
+    public static boolean isEmpty(Collection<?> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    /**
+     * 判断集合是否不为空
+     *
+     * @param collection 集合
+     * @return 是否不为空
+     */
+    public static boolean isNotEmpty(Collection<?> collection) {
+        return !isEmpty(collection);
+    }
+
+    /**
+     * 判断Map是否为空
+     *
+     * @param map Map
+     * @return 是否为空
+     */
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+
+    /**
+     * 判断Map是否不为空
+     *
+     * @param map Map
+     * @return 是否不为空
+     */
+    public static boolean isNotEmpty(Map<?, ?> map) {
+        return !isEmpty(map);
+    }
+
 }
