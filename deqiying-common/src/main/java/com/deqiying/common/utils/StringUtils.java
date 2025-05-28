e660c338af60daf055c9758ab141d83a1351d348
deqiying <deqiying@gmail.com>
2025-05-28 15:24:00 +0800

    feat(StringUtils): 新增方法-大小写反转

--- deqiying-common/src/main/java/com/deqiying/common/utils/StringUtils.java
+++ deqiying-common/src/main/java/com/deqiying/common/utils/StringUtils.java
@@ -562,7 +562,6 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
     }
 
 
-
     /**
      * 清理字符串特殊字符：移除特殊符号、颜文字和空格
      *
@@ -629,6 +628,36 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
             padding[i] = padChars[i % padLen];
         }
         return str.concat(new String(padding));
+    }
+
+    /**
+     * 大小写反转
+     *
+     * @param str 字符串
+     * @return 反转后的字符串
+     */
+    public static String swapCase(final String str) {
+        if (isEmpty(str)) {
+            return str;
+        }
+
+        final int strLen = str.length();
+        final int[] newCodePoints = new int[strLen]; // cannot be longer than the char array
+        int outOffset = 0;
+        for (int i = 0; i < strLen; ) {
+            final int oldCodepoint = str.codePointAt(i);
+            final int newCodePoint;
+            if (Character.isUpperCase(oldCodepoint) || Character.isTitleCase(oldCodepoint)) {
+                newCodePoint = Character.toLowerCase(oldCodepoint);
+            } else if (Character.isLowerCase(oldCodepoint)) {
+                newCodePoint = Character.toUpperCase(oldCodepoint);
+            } else {
+                newCodePoint = oldCodepoint;
+            }
+            newCodePoints[outOffset++] = newCodePoint;
+            i += Character.charCount(newCodePoint);
+        }
+        return new String(newCodePoints, 0, outOffset);
     }
 
 }
